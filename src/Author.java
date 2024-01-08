52b777d99fe59bf76224bb9f30026cf1079dcb4b
Sfa11eN <sfallenn@gmail.com>
2024-01-08 16:41:41 +0300

    final commit

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+
+    public String firstName;
+
+    public String secondName;
+
+    public Author(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+}
